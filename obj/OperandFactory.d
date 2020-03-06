2ec701c9bf01df931a4008c60fec4e55824c303f
MagicHatJo <jnochang@gmail.com>
2020-03-06 11:28:12 -0800

    cleaned up repo

--- obj/OperandFactory.d
+++
@@ -1,2 +0,0 @@
-obj/OperandFactory.o: src/OperandFactory.cpp inc/OperandFactory.hpp \
- inc/eOperandType.hpp inc/IOperand.hpp inc/Operand.hpp
--- obj/main.d
+++
@@ -1,3 +0,0 @@
-obj/main.o: src/main.cpp inc/AbstractVM.hpp inc/ThreadQueue.hpp \
- inc/Token.hpp inc/eOperandType.hpp inc/IOperand.hpp \
- inc/OperandFactory.hpp inc/Operand.hpp
